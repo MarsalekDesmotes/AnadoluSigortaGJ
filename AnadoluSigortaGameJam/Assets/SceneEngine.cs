4a1f894352140e682dc964df0aa959f564eab8f5
MarsalekDesmotes <57544847+MarsalekDesmotes@users.noreply.github.com>
2022-09-30 04:18:35 +0300

    BookMernu

---
+++ AnadoluSigortaGameJam/Assets/SceneEngine.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneEngine : MonoBehaviour
+{
+    // Start is called before the first frame update
+   public void Play()
+    {
+
+        SceneManager.LoadScene("Level 1");
+
+    }
+
+    public void Credits()
+    {
+
+        SceneManager.LoadScene("Credits");
+
+    }
+
+    public void Exit()
+    {
+
+        Application.Quit();
+
+    }
+}
